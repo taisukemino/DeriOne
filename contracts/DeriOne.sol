79869def135fea7d01a2a2c67cfc1cee5235513f
Tai <4382091+taisukemino@users.noreply.github.com>
2020-12-08 11:42:00 +0900

    add functions to instantiate contracts

---
+++ contracts/DeriOne.sol
@@ -0,0 +1,37 @@
+pragma solidity 0.6.0;
+
+import "./interfaces/IHegicETHOption.sol";
+import "./interfaces/IETHPriceOracle.sol";
+import "./interfaces/IOpynExchange.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract DeriOne is Ownable {
+    IHegicETHOption private IHegicETHOptionInstance;
+    IETHPriceOracle private IETHPriceOracleInstance;
+    IOpynExchange private IOpynExchangeInstance;
+
+    event NewHegicETHOptionAddressRegistered(address hegicETHOptionAddress);
+    event NewETHPriceOracleAddressRegistered(address ETHPriceOracleAddress);
+    event NewOpynExchangeAddressRegistered(address opynExchangeAddress);
+    setETHPriceOracleAddress(address _ETHPriceOracleAddress) public onlyOwner {
+        ETHPriceOracleAddress = _ETHPriceOracleAddress;
+        IETHPriceOracleInstance = IETHPriceOracle(ETHPriceOracleAddress);
+
+        emit NewETHPriceOracleAddressRegistered(ETHPriceOracleAddress);
+    }
+
+    setHegicETHOptionAddress(address _hegicETHOptionAddress) public onlyOwner {
+        hegicETHOptionAddress = _hegicETHOptionAddress;
+        IHegicETHOptionInstance = IHegicETHOption(hegicETHOptionAddress);
+
+        emit NewHegicETHOptionAddressRegistered(hegicETHOptionAddress);
+    }
+
+    setOpynExchangeAddress(address _opynExchangeAddress) public onlyOwner {
+        opynExchangeAddress = _opynExchangeAddress;
+        IOpynExchangeInstance = IOpynExchange(opynExchangeAddress);
+
+        emit NewOpynExchangeAddressRegistered(opynExchangeAddress);
+    }
+
+}
