757ea61ef7326ef6e935bfacde8af3678f31df4c
Tai <4382091+taisukemino@users.noreply.github.com>
2020-12-09 11:29:14 +0900

    square function

--- contracts/DeriOne.sol
+++ contracts/DeriOne.sol
@@ -88,6 +88,22 @@ contract DeriOne is Ownable {
         return ETHPrice;
     }
 
+    /// @notice babylonian method
+    /// @param y unsigned integer 256
+    /// modified https://github.com/Uniswap/uniswap-v2-core/blob/4dd59067c76dea4a0e8e4bfdda41877a6b16dedc/contracts/libraries/Math.sol#L11
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y.div(2) + 1;
+            while (x < z) {
+                z = x;
+                x = (y.div(x) + x).div(2);
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
     /// @notice calculate the premium in hegic
     function getHegicPremium(period, strike) {
         uint256 impliedVolatility = getImpliedVolatility();
