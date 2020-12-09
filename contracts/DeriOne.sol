8f4a30a1faefc00e262e8b5b799dfd05d760ef0e
Tai <4382091+taisukemino@users.noreply.github.com>
2020-12-09 18:04:15 +0900

    some token address variables

--- contracts/DeriOne.sol
+++ contracts/DeriOne.sol
@@ -20,6 +20,9 @@ contract DeriOne is Ownable {
     IOpynOptionsFactoryV1 private IOpynOptionsFactoryV1Instance;
     IOpynOTokenV1 private IOpynOTokenV1Instance;
 
+    address constant USDCTokenAddress = 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48;
+    address constant WETHTokenAddress = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2;
+
     address oTokenAddressList [];
 
     event NewETHPriceOracleAddressRegistered(address ETHPriceOracleAddress);
