aae3c1320ad49b3df9a036d6a82aa6814369be45
Tai <4382091+taisukemino@users.noreply.github.com>
2020-12-24 00:44:35 +0900

    the version name to a main file

--- contracts/DeriOneV1.sol
+++ contracts/DeriOneV1.sol
@@ -498,8 +498,6 @@ contract DeriOne is Ownable {
         );
     }
 
-    /// @notice buy the cheapest ETH put option
-    /// @param receiver the account that will receive the oTokens
     function buyTheCheapestETHPutOption(
         uint256 minExpiry,
         uint256 maxExpiry,
@@ -533,3 +531,45 @@ contract DeriOne is Ownable {
         }
     }
 }
+
+// watch new eth put options being created
+// how do you compare options?
+// what happens after our user buy? don't they want to exercise?
+// we could make two functions: one gets some options and the other gets only one option
+// console log solidity https://medium.com/nomic-labs-blog/better-solidity-debugging-console-log-is-finally-here-fc66c54f2c4a
+// event, logx or
+// truffle console log?
+// make two functions. one that takes a range and the other that takes a fixed value. what to return when there is none?
+// ask them: mazzi, gammahammer are both options traders irl. attm is a maths phd that prices options for living
+// there are two functions: fixed expiry and strike function. in opyn, it is either nothing or exist. in hegic, it always returns only one.
+// fixed values: in opyn, it is like that you can get options. in hegic, np.
+// people perhaps want to buy the most liquid one so that they can make sure that they can sell it later?
+// specify data location
+// enforce state changes of state variables with a function by adding a storage keyword? i dont think so.
+// adjust variable visibility
+// think how functions call each other
+// why say memory or calldata in a parameter?
+// calldata and stack dont understand
+// value type and reference type?
+// stack and heap?
+// think of a new way to structure your variables
+// start function parameter variable names with an `underscore(_)` to differentiate them from global variables.
+// add some function modifiers like view and pure?
+// you cannot rely on abi to interface converter. it is not good. i made more than enough mistakes in the interfaces.
+// explicitly state the data location for all variables of struct, array or mapping types (including function parameters)
+// adjust visibility of variables. they should be all private by default i guess
+// the way i handle otoken instances are wrong. this needs to be fixed.
+
+// What to do with that strikePrice that returns two values. What do they do?
+//    /* represents floting point numbers, where number = value * 10 ** exponent
+//     i.e 0.1 = 10 * 10 ** -3 */
+//     struct Number {
+//         uint256 value;
+//         int32 exponent;
+//     }
+
+// split this contract file into some files
+
+// refer to uniswap factory contract to manage registry? 
+
+// perhaps hasLiquidity can be a modifier?
