b8c905dc0bf0f617d85409148ccc50ea6d0ae7c7
Tai <4382091+taisukemino@users.noreply.github.com>
2020-12-24 12:49:40 +0900

    import the SafeMath in the Math library

--- contracts/libraries/Math.sol
+++ contracts/libraries/Math.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.6.0;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
 library Math {
+    using SafeMath for uint256;
+
     /// @notice babylonian method
     /// @param y unsigned integer 256
     /// modified https://github.com/Uniswap/uniswap-v2-core/blob/4dd59067c76dea4a0e8e4bfdda41877a6b16dedc/contracts/libraries/Math.sol#L11
