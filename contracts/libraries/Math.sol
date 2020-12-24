dd49541f964d6c349d3c97dfe75d6db7c60398dc
Tai <4382091+taisukemino@users.noreply.github.com>
2020-12-24 12:49:56 +0900

    make the sqrt method internal

--- contracts/libraries/Math.sol
+++ contracts/libraries/Math.sol
@@ -8,7 +8,7 @@ library Math {
     /// @notice babylonian method
     /// @param y unsigned integer 256
     /// modified https://github.com/Uniswap/uniswap-v2-core/blob/4dd59067c76dea4a0e8e4bfdda41877a6b16dedc/contracts/libraries/Math.sol#L11
-    function _sqrt(uint256 y) private pure returns (uint256 z) {
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
         if (y > 3) {
             z = y;
             uint256 x = y.div(2) + 1;
