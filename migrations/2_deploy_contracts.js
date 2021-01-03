4fd4d3029c61e0c0a2e2f0c88170b672b2b42d0b
Tai <4382091+taisukemino@users.noreply.github.com>
2021-01-03 09:42:08 +0900

    edit the deploy contract migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,23 @@
 const DeriOneV1Main = artifacts.require("DeriOneV1Main.sol");
 
 module.exports = function (deployer, network) {
-  if (network === "development") {
+  // const variables for the constructor
+  const ETHPriceOracleAddress = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419";
+  const hegicETHOptionV888Address =
+    "0xEfC0eEAdC1132A12c9487d800112693bf49EcfA2";
+  const hegicETHPoolV888Address = "";
+  const opynExchangeV1Address = "0x39246c4f3f6592c974ebc44f80ba6dc69b817c71";
+  const opynOptionsFactoryV1Address = "";
+  const uniswapFactoryV1Address = "";
+
+  if (network === "develop") {
     deployer.deploy(DeriOneV1Main, arg1, arg2);
   } else if (network === "mainnet") {
-    // const variables for the constructor
-    const ETHPriceOracleAddress = "";
-    const ETHPriceOracleAddress = "";
-    const hegicETHPoolV888Address = "";
-    const opynExchangeV1Address = "";
-    const opynOptionsFactoryV1Address = "";
-    const uniswapFactoryV1Address = "";
     // deploy the DeriOneV1Main contract with constructor arguments
     deployer.deploy(
       DeriOneV1Main,
       ETHPriceOracleAddress,
-      ETHPriceOracleAddress,
+      hegicETHOptionV888Address,
       hegicETHPoolV888Address,
       opynExchangeV1Address,
       opynOptionsFactoryV1Address,
