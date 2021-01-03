104be9ff4bd71c1c039fe70074abe431e6f0b014
Tai <4382091+taisukemino@users.noreply.github.com>
2021-01-03 10:19:32 +0900

    edit a migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,27 +1,24 @@
 const DeriOneV1Main = artifacts.require("DeriOneV1Main.sol");
 
-module.exports = function (deployer, network) {
+module.exports = function (deployer) {
   // const variables for the constructor
   const ETHPriceOracleAddress = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419";
   const hegicETHOptionV888Address =
     "0xEfC0eEAdC1132A12c9487d800112693bf49EcfA2";
-  const hegicETHPoolV888Address = "";
+  const hegicETHPoolV888Address = "0x878f15ffc8b894a1ba7647c7176e4c01f74e140b";
   const opynExchangeV1Address = "0x39246c4f3f6592c974ebc44f80ba6dc69b817c71";
-  const opynOptionsFactoryV1Address = "";
-  const uniswapFactoryV1Address = "";
+  const opynOptionsFactoryV1Address =
+    "0xcc5d905b9c2c8c9329eb4e25dc086369d6c7777c";
+  const uniswapFactoryV1Address = "0xc0a47dfe034b400b47bdad5fecda2621de6c4d95";
 
-  if (network === "teams") {
-    deployer.deploy(DeriOneV1Main, arg1, arg2);
-  } else if (network === "mainnet") {
-    // deploy the DeriOneV1Main contract with constructor arguments
-    deployer.deploy(
-      DeriOneV1Main,
-      ETHPriceOracleAddress,
-      hegicETHOptionV888Address,
-      hegicETHPoolV888Address,
-      opynExchangeV1Address,
-      opynOptionsFactoryV1Address,
-      uniswapFactoryV1Address
-    );
-  }
+  // deploy the DeriOneV1Main contract with constructor arguments
+  deployer.deploy(
+    DeriOneV1Main,
+    ETHPriceOracleAddress,
+    hegicETHOptionV888Address,
+    hegicETHPoolV888Address,
+    opynExchangeV1Address,
+    opynOptionsFactoryV1Address,
+    uniswapFactoryV1Address
+  );
 };
